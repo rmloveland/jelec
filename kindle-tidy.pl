677820b58029db0a2283346ec7c0825c811f8a4f
Rich Loveland <loveland.richard@gmail.com>
2011-12-20 13:19:21 -0500

    Kindle-tidy now removes 'Figure N' elements from generated HTML

--- kindle-tidy.pl
+++ kindle-tidy.pl
@@ -2,13 +2,13 @@
 
 use strict;
 use warnings;
+use 5.010;
 use HTML::TreeBuilder;
 
 my $root = HTML::TreeBuilder->new;
 $root->parse_file(shift);
 
 my @divs = $root->find_by_tag_name('div');
-
 foreach my $div (@divs) {
   if ($div->attr_get_i('id') eq 'postamble') {
     $div->delete_content;
@@ -18,6 +18,16 @@ foreach my $div (@divs) {
 my @tables = $root->find_by_tag_name('table');
 foreach my $table (@tables) {
   $table->delete_content;
+}
+
+my @paras = $root->find_by_tag_name('p');
+foreach my $para (@paras) {
+  my @children = $para->content_list;
+  for my $child (@children) {
+    if ($child =~ /Figure \d+/i) {
+      $para->delete_content;
+    }
+  }
 }
 
 print $root->as_HTML;
